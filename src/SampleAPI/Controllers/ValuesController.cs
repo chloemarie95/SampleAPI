e3e90d853df53b14dc8fe63d733960bec4fd94c2
Chloe McDermott <chloemcdermott95@aol.com>
2019-08-23 21:18:19 +0100

    changes return values for test

--- src/SampleAPI/Controllers/ValuesController.cs
+++ src/SampleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SampleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "dotnet", "API test" };
         }
 
         // GET api/values/5
