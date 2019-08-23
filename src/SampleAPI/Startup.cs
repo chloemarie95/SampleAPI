204ba04d32c5437616a84d2bea912be0750f1833
Chloe McDermott <chloemcdermott95@aol.com>
2019-08-23 20:17:42 +0100

    Removed https redirection and updated package version

--- src/SampleAPI/Startup.cs
+++ src/SampleAPI/Startup.cs
@@ -40,7 +40,7 @@ namespace SampleAPI
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
